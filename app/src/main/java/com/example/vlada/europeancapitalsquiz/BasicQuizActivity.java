ca71de304237abdc22112bf946f55d20ad2ac786
Solidfire <34316981+AlexandruVld@users.noreply.github.com>
2017-12-24 05:06:16 +0200

    Improved code

--- app/src/main/java/com/example/vlada/europeancapitalsquiz/BasicQuizActivity.java
+++ app/src/main/java/com/example/vlada/europeancapitalsquiz/BasicQuizActivity.java
@@ -18,30 +18,7 @@ public class BasicQuizActivity extends AppCompatActivity {
     //This method is called when the submit button is clicked
     public void submitAnswers (View view){
 
-        //Checks which radio button is checked in question 1.
-        RadioButton zagreb = (RadioButton)findViewById(R.id.albania_zagreb);
-        boolean selectZagreb = zagreb.isChecked();
-        RadioButton tirana = (RadioButton)findViewById(R.id.albania_tirana);
-        boolean selectTirana = tirana.isChecked();
-        RadioButton tibilisi = (RadioButton)findViewById(R.id.albania_tibilisi);
-        boolean selectTibilisi = tibilisi.isChecked();
-
-        //Checks which radio button is checked in question 2.
-        RadioButton stockholm = (RadioButton)findViewById(R.id.iceland_stockholm);
-        boolean selectStockholm = stockholm.isChecked();
-        RadioButton helsinki = (RadioButton)findViewById(R.id.iceland_helsinki);
-        boolean selectHelsinki = helsinki.isChecked();
-        RadioButton reykjavik = (RadioButton)findViewById(R.id.iceland_reykjavik);
-        boolean selectReykjavik = reykjavik.isChecked();
-
-        //Checks which radio button is checked in question 3.
-        RadioButton bratislava = (RadioButton)findViewById(R.id.slovakia_bratislava);
-        boolean selectBratislava = bratislava.isChecked();
-        RadioButton ljublijana = (RadioButton)findViewById(R.id.slovakia_ljublijana);
-        boolean selectljublijana = ljublijana.isChecked();
-        RadioButton belgrade = (RadioButton)findViewById(R.id.slovakia_belgrade);
-        boolean selectBelgrade = belgrade.isChecked();
-        int finalScore = calculatePoints(selectTirana, selectReykjavik, selectBratislava);
+        int finalScore = calculatePoints();
 
             //displays this toast if the user acumulated 3 points
             if (finalScore == 3){
@@ -62,23 +39,23 @@ public class BasicQuizActivity extends AppCompatActivity {
      *@param checkedBratislava is wether the user checked the corect answer
     */
 
-    public int calculatePoints (boolean checkedReykjavik, boolean checkedTirana, boolean checkedBratislava) {
+    public int calculatePoints () {
 
         //initial value of the points
         int score = 0;
 
         //checks if the user answered correct the first question
-        if (checkedTirana) {
+        if (((RadioButton)findViewById(R.id.albania_tirana)).isChecked()) {
             score += 1;
         }
 
         //checks if the user answered correct the second question
-        if (checkedReykjavik) {
+        if (((RadioButton)findViewById(R.id.iceland_reykjavik)).isChecked()) {
             score += 1;
         }
 
         //checks if the user answered correct the third question
-        if (checkedBratislava){
+        if (((RadioButton)findViewById(R.id.slovakia_bratislava)).isChecked()){
             score += 1;
         }
 
--- app/src/main/java/com/example/vlada/europeancapitalsquiz/EasyActivity.java
+++ app/src/main/java/com/example/vlada/europeancapitalsquiz/EasyActivity.java
@@ -18,30 +18,7 @@ public class EasyActivity extends AppCompatActivity {
     //This method is called when the submit button is clicked
     public void submitAnswers (View view){
 
-        //Checks which radio button is checked in question 1.
-        RadioButton paris = (RadioButton)findViewById(R.id.france_paris);
-        boolean selectParis = paris.isChecked();
-        RadioButton bucharest = (RadioButton)findViewById(R.id.france_bucharest);
-        boolean selectBucharest = bucharest.isChecked();
-        RadioButton prague = (RadioButton)findViewById(R.id.france_prague);
-        boolean selectPrague = prague.isChecked();
-
-        //Checks which radio button is checked in question 2.
-        RadioButton dublin = (RadioButton)findViewById(R.id.germany_dublin);
-        boolean selectDublin = dublin.isChecked();
-        RadioButton berlin = (RadioButton)findViewById(R.id.germany_berlin);
-        boolean selectBerlin = berlin.isChecked();
-        RadioButton rome = (RadioButton)findViewById(R.id.germany_rome);
-        boolean selectRome = rome.isChecked();
-
-        //Checks which radio button is checked in question 3.
-        RadioButton athens = (RadioButton)findViewById(R.id.england_athens);
-        boolean selectAthens = athens.isChecked();
-        RadioButton madrid = (RadioButton)findViewById(R.id.england_madrid);
-        boolean selectMadrid = madrid.isChecked();
-        RadioButton london = (RadioButton)findViewById(R.id.england_london);
-        boolean selectLondon = london.isChecked();
-        int finalScore = calculatePoints(selectParis, selectBerlin, selectLondon);
+        int finalScore = calculatePoints();
 
         //displays this toast if the user acumulated 3 points
         if (finalScore == 3){
@@ -62,23 +39,23 @@ public class EasyActivity extends AppCompatActivity {
      *@param checkedBratislava is wether the user checked the corect answer
     */
 
-    public int calculatePoints (boolean checkedParis, boolean checkedBerlin, boolean checkedLondon) {
+    public int calculatePoints () {
 
         //initial value of the points
         int score = 0;
 
         //checks if the user answered correct the first question
-        if (checkedParis) {
+        if (((RadioButton)findViewById(R.id.france_paris)).isChecked()) {
             score += 1;
         }
 
         //checks if the user answered correct the second question
-        if (checkedBerlin) {
+        if (((RadioButton)findViewById(R.id.germany_berlin)).isChecked()) {
             score += 1;
         }
 
         //checks if the user answered correct the third question
-        if (checkedLondon){
+        if (((RadioButton)findViewById(R.id.england_london)).isChecked()){
             score += 1;
         }
 
